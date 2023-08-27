408cd6464927866099958f03132576aff2e585f6
Vanderlei Dallagnolo <vanderlei.dallagnolo@ellevo.com>
2023-08-27 04:32:44 -0300

    .

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Hello = () => {
-    return (
-        <div>
-            <h1>Hello Vishwas</h1>
-        </div>
-    )
+    // return (
+    //     <div>
+    //         <h1>Hello Vishwas</h1>
+    //     </div>
+    // )
 }
 
 export default Hello;
