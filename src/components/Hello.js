c546e33a49d0db11a98fec74f8af9b6f0627adbf
Vanderlei Dallagnolo <vanderlei.dallagnolo@ellevo.com>
2023-08-27 04:44:41 -0300

    .

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -6,6 +6,12 @@ const Hello = () => {
     //         <h1>Hello Vishwas</h1>
     //     </div>
     // )
+
+    return React.createElement(
+        'div',
+        {id: 'hello', className: 'dummyClass'},
+        React.createElement('h1', null, 'Hello Vishwas')
+    )
 }
 
 export default Hello;
