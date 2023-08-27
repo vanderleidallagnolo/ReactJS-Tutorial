7404edb6b4ce3493e58ddce6583375dd366d728d
Vanderlei Dallagnolo <vanderlei.dallagnolo@ellevo.com>
2023-08-27 11:01:38 -0300

    .

--- src/components/EventBind.js
+++ src/components/EventBind.js
@@ -1,11 +1,67 @@
 import React, { Component } from 'react'
 
 class EventBind extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            message1: 'Hello',
+            message2: 'Hello',
+            message3: 'Hello'
+        }
+       this.clickHandler3 = this.clickHandler3.bind(this);
+
+
+    }
+    
+    clickHandler1()  {
+        this.setState(
+            {
+                message1: 'Goodbye!'
+            }
+        )
+    }
+
+    clickHandler2()  {
+        this.setState(
+            {
+                message2: 'Goodbye!'
+            }
+        )
+    }
+
+    clickHandler3()  {
+        this.setState(
+            {
+                message3: 'Goodbye!'
+            }
+        )
+    }
+
+
+
   render() {
     return (
-      <div>
-        
-      </div>
+
+        <>
+            <div>
+                <div>EventBind - First Approach</div>
+                <div>{this.state.message1}</div>
+                <button onClick={this.clickHandler1.bind(this)}>Click me</button>
+            </div>
+            <div>
+                <div>EventBind - Second Approach</div>
+                <div>{this.state.message2}</div>
+                <button onClick={() => this.clickHandler2()}>Click me</button>
+            </div>
+            <div>
+                <div>EventBind - ThirdApproach</div>
+                <div>{this.state.message3}</div>
+                <button onClick={this.clickHandler3}>Click me</button>
+            </div>
+        </>
+       
     )
   }
 }
